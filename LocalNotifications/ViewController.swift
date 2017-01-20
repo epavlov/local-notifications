8f5839f4b7e1a44f32e238ba473e944617f31ae1
Eugene Pavlov <eugenepavlov@Eugenes-MacBook-Pro.local>
2017-01-20 09:18:59 -0500

    initial commit

---
+++ LocalNotifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalNotifications
+//
+//  Created by Eugene Pavlov on 1/20/17.
+//  Copyright © 2017 Eugene Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
